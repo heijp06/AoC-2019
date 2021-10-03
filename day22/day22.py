4103459d6fd85658333a1357c159c8cfe8410b54
Peter van der Heijden <peter@vanderheijden.name>
2021-10-03 13:33:14 +0200

    Refactor a little.

--- day22/day22.py
+++ day22/day22.py
@@ -1,11 +1,13 @@
 from lib import part1, part2
 import csv
 
-def read_rows():
-    with open('data.txt', newline='') as csv_file:
-        return list(csv.reader(csv_file, delimiter=' '))
 
-rows = read_rows()
+def read_rows(**kwargs):
+    with open('data.txt', newline='') as csv_file:
+        return list(csv.reader(csv_file, **kwargs))
+
+
+rows = read_rows(delimiter=' ')
 
 print(f"Part 1: {part1(rows)}")
 print(f"Part 2: {part2(rows)}")
--- day22/lib.py
+++ day22/lib.py
@@ -1,4 +1,6 @@
 NUMBER_OF_CARDS = 10007
+
+
 def part1(rows):
     pos = 2019
     for row in rows:
@@ -11,6 +13,7 @@ def part1(rows):
             value = int(row[-1])
             pos = (pos * value) % NUMBER_OF_CARDS
     return pos
+
 
 def part2(rows):
     pass
