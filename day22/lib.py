35811569b22169d668e7f226db825e4eb05c5540
Peter van der Heijden <peter@vanderheijden.name>
2021-10-03 17:28:05 +0200

    Solve day 22 part 2.

--- day22/lib.py
+++ day22/lib.py
@@ -4,6 +4,14 @@ def part1(actions):
     return (2019 * scale + shift) % NUMBER_OF_CARDS
 
 
+def part2(actions):
+    NUMBER_OF_CARDS = 119315717514047
+    NUMBER_OF_SHUFFLES = 101741582076661
+    scale, shift = shuffle(actions, NUMBER_OF_CARDS)
+    scale, shift = repeat(scale, shift, NUMBER_OF_SHUFFLES, NUMBER_OF_CARDS)
+    return ((2020 - shift) * pow(scale, -1, NUMBER_OF_CARDS)) % NUMBER_OF_CARDS
+
+
 def shuffle(actions, number_of_cards):
     scale = 1
     shift = 0
@@ -23,5 +31,8 @@ def shuffle(actions, number_of_cards):
     return scale, shift
 
 
-def part2(actions):
-    pass
+def repeat(scale, shift, number_of_shuffles, number_of_cards):
+    new_scale = pow(scale, number_of_shuffles, number_of_cards)
+    inv_scale_minus_one = pow(scale - 1, -1, number_of_cards)
+    new_shift = (shift * inv_scale_minus_one * (new_scale - 1)) % number_of_cards
+    return new_scale, new_shift
--- day22/test_lib.py
+++ day22/test_lib.py
@@ -1,4 +1,4 @@
-from lib import shuffle
+from lib import repeat, shuffle
 import pytest
 
 result_4 = [9, 2, 5, 8, 1, 4, 7, 0, 3, 6]
@@ -20,3 +20,7 @@ def test_shuffle_example_4(card_number):
     ]
     scale, shift = shuffle(actions, 10)
     assert (scale * card_number + shift) % 10 == result_4.index(card_number)
+
+def test_repeat():
+    scale, shift = repeat(2, 3, 4, 10)
+    assert (scale, shift) == (6, 5)
